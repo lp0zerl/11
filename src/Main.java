480c809b2948f137831226f3edc4c754fbe143f3
Lenovo <kontumer16@gmail.com>
2025-03-25 13:15:10 +0300

    все задания

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,5 @@
-class Author {
-    private String firstName;
-    private String lastName;
-
-    public Author(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-}
-
-class Book {
-    private String title;          // Название книги
-    private Author author;         // Автор книги
-    private int publicationYear;   // Год публикации
-
-    public Book(String title, Author author, int publicationYear) {
-        this.title = title;
-        this.author = author;
-        this.publicationYear = publicationYear;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public int getPublicationYear() {
-        return publicationYear;
-    }
-
-    public void setPublicationYear(int publicationYear) {
-        this.publicationYear = publicationYear;
-    }
-}
+import model.Author;
+import model.Book;
 
 public class Main {
     public static void main(String[] args) {
